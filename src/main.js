61b5a4fcc1c45a78bbdb37cadffa2fab09a36ed2
Svietlova <139239860+Svietlova@users.noreply.github.com>
2024-03-14 10:51:34 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '../src/js/header';
+import '../src/js/hero';
+import '../src/js/about-me';
+import '../src/js/benefits';
+import '../src/js/projects';
+import '../src/js/faq';
+import '../src/js/covers';
+import '../src/js/footer';
