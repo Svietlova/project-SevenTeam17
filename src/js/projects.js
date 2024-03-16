99712a956bee72c2da1ade3f69a3d6d693a08ea7
Polina Proshkina <polinaproshkina1998@gmail.com>
2024-03-16 17:48:13 +0100

    Projects section

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,1 +1,18 @@
 console.log("projects");
+
+import Swiper from 'swiper';
+import '../../node_modules/swiper/swiper.css';
+
+const btnNext = document.querySelector('.projects_slider_btn--right');
+const btnPrev = document.querySelector('.projects_slider_btn--left');
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+});
+
+btnNext.addEventListener('click', () => { swiper.slideNext() });
+btnPrev.addEventListener('click', () => { swiper.slidePrev() });
+
+
+
