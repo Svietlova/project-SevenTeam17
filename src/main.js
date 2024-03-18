9420c4a6afb1a4d783d10d273fbd57cfb6db750e
Svietlova <irasvet1997@gmail.com>
2024-03-18 22:36:55 +0200

    раскомментирован about me

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import '../src/js/header';
 import '../src/js/hero';
-// import '../src/js/about-me';
+import '../src/js/about-me';
 import '../src/js/benefits';
 import '../src/js/projects';
 import '../src/js/faq';
