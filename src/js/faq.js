4e10170c48b29c378d34fb1c51cb86b4653a8e78
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-03-19 00:41:53 +0200

    new-changes-in-styles

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,1 +1,16 @@
 console.log("faq");
+
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const faqButton = document.querySelectorAll('.faq-button');
+const text = document.querySelector('.faq-text');
+
+faqButton.forEach(button => {
+    console.log(button);
+    button.addEventListener('click', (event) => {
+        if (event.currentTarget) {
+            text.classList.add('');
+        }
+    })
+})
