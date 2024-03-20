5e9ace6d8c834d140f7d7a8a7d06c979d97c243c
Svietlova <irasvet1997@gmail.com>
2024-03-20 22:36:03 +0200

    js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,7 @@
 console.log('work together');
 
-import imgLogo from '../img/header-footer/jeff-logo.png';
-import sprite from '../img/sprite.svg';
+// import imgLogo from '../img/header-footer/jeff-logo.png';
+// import sprite from '../img/sprite.svg';
 
 function removeSuccess(input) {
   const parent = input.parentNode;
