9c61d538529fa4622e9259b33734642f7de0f499
Svietlova <irasvet1997@gmail.com>
2024-03-20 21:50:16 +0200

    styles-about.css

--- src/js/header.js
+++ src/js/header.js
@@ -20,7 +20,7 @@ console.log("header");
       const scrollLockMethod = !isMenuOpen
         ? 'disableBodyScroll'
         : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+      // bodyScrollLock[scrollLockMethod](document.body);
     };
   
     openMenuBtn.addEventListener('click', toggleMenu);
@@ -33,7 +33,7 @@ console.log("header");
       if (!e.matches) return;
       mobileMenu.classList.remove('is-open');
       openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
+      // bodyScrollLock.enableBodyScroll(document.body);
     });
   })();
   
