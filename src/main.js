7581db32761bb3773b7ed50ce129fc41f10116a7
Svietlova <irasvet1997@gmail.com>
2024-03-22 14:28:42 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import '../src/js/header';
-import '../src/js/hero';
 import '../src/js/about-me';
-import '../src/js/benefits';
 import '../src/js/projects';
 import '../src/js/faq';
 import '../src/js/covers';
